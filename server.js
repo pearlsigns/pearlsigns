7f197f7bc3c27f19326b5bd7b5db56c408ba6be9
valairan <darylvalairan@outlook.com>
2024-11-28 13:31:30 -0500

    Using Router

--- server.js
+++ server.js
@@ -13,51 +13,52 @@ app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 //app.listen(8080, '10.0.0.62');
 //app.listen(8080);
-module.exports.handler = serverless(app);
 
 app.use("/.netlify/functions/app", router);
 
 
 // *** GET Routes - display pages ***
 // Root Route
-app.get('/', function (req, res) {
+router.get('/', function (req, res) {
     res.render('pages/index');
 });
 
-app.get('/index', function (req, res) {
+router.get('/index', function (req, res) {
     res.render('pages/index');
 });
 
-app.get('/about', function (req, res) {
+router.get('/about', function (req, res) {
     res.render('pages/about');
 });
 
-app.get('/services', function (req, res) {
+router.get('/services', function (req, res) {
     res.render('pages/services');
 });
 
-app.get('/contact', function (req, res) {
+router.get('/contact', function (req, res) {
     res.render('pages/contact');
 });
 
-app.get('/project', function (req, res) {
+router.get('/project', function (req, res) {
     res.render('pages/project');
 });
 
-app.get('/auth', function (req, res) {
+router.get('/auth', function (req, res) {
     res.render('pages/auth');
 });
 
-app.post('/firebase/auth', function(req, res){
+router.post('/firebase/auth', function(req, res){
     res.json({"message": "ok"});
 });
 
-app.post('/firebase/visit', async function(req, res){
+router.post('/firebase/visit', async function(req, res){
     firebase.updateVisits();
     console.log("Someone viited")
     res.json({"message": "ok"});
 });
 
-app.post('/firebase/quote', function(req, res){
-
+router.post('/firebase/quote', function(req, res){
+    
 });
+
+module.exports.handler = serverless(app);
